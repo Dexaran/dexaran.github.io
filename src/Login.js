96c3bd50f3c5de4ba9dc77990d2a401f5e9fcb94
Dexaran <dexaran820@gmail.com>
2019-09-29 13:20:34 +0300

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+      
+    return (
+        <div className="container py-5">
+        
+      		<div className="row">
+              
+      			<div className="col-10 mx-auto text-center text-slanted text-black my-5">
+      				
+                      <h2>Login</h2>
+
+        
+                </div>
+            </div>
+        </div>
+        
+    );
+  }
+}
+ 
+export default Login;
