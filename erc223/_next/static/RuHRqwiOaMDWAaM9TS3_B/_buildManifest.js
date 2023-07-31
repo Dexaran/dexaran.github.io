7e713624becec5afa82bcd8db755ddacb74e8e08
Dexaran <dexaran820@gmail.com>
2023-08-01 02:30:26 +0400

    Add files via upload

---
+++ erc223/_next/static/RuHRqwiOaMDWAaM9TS3_B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e4abf842d9816f4b.css","static/chunks/pages/index-fe5725fcbd7c7bb8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
