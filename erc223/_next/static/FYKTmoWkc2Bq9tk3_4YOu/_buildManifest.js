4daae773ffe73f7019567d967e2c773917bfc160
Dexaran <dexaran820@gmail.com>
2023-08-01 15:24:23 +0000

    Deploying to master from @ Dexaran/dexaran.github.io@425f339b67388abc29f7aa657fb3532284fa1a9f 🚀

--- erc223/_next/static/FYKTmoWkc2Bq9tk3_4YOu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b9dd42a7ca23747f.css","static/chunks/pages/index-1d74fe757430fce9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
