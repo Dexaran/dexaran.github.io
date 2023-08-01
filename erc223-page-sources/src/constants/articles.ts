f4b485600d97e86ec6fa1111f65b6020a73ffc3e
Dexaran <dexaran820@gmail.com>
2023-08-01 16:38:23 +0400

    Update articles.ts

--- erc223-page-sources/src/constants/articles.ts
+++ erc223-page-sources/src/constants/articles.ts
@@ -8,92 +8,13 @@ export const articles: IArticle[] = [
     image: "image1.png"
   },
   {
-    title: "Known problems of ERC-20 standard.",
+    title: "ERC-20 approve & transferFrom methods pose a threat to safety of users funds",
     description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
+    url: "https://dexaran820.medium.com/erc-20-approve-transferfrom-asset-transfer-method-poses-a-threat-to-users-funds-safety-ff7195127018"
   },
   {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },{
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Known problems of ERC-20 standard.",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
-  },
-  {
-    title: "Example of adding new article",
-    description: "This record is added in development purpose",
-    url: "https://google.com"
+    title: "Callisto Security statement regarding ERC-20 standard.",
+    description: "",
+    url: "https://callisto.network/erc-20-standard-callisto-network-security-department-statement/"
   }
 ];
