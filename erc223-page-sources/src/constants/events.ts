bee41f7a1d02b5a630c2245e035b74ffa09c86da
Dexaran <dexaran820@gmail.com>
2023-08-05 01:47:17 +0400

    Update events.ts

--- erc223-page-sources/src/constants/events.ts
+++ erc223-page-sources/src/constants/events.ts
@@ -2,32 +2,77 @@ import {IRecentEvent} from "../types";
 
 export const events: IRecentEvent[] = [
   {
-    title: "[github] ERC-223 copy was submitted as ECIP to Ethereum Classic",
-    url: "https://github.com/ethereumproject/ECIPs/issues/53",
-    date: "18.05.2017",
-    id: "tag_eip_submission",
+    title: "[ETH reddit] First in the world ERC-223 ICO launched",
+    url: "https://www.reddit.com/r/ethereum/comments/6m2vvn/the_first_erc223_ico_is_now_launched_on/",
+    date: "8.07.2017",
   },
   {
-    title: "[Ethereum reddit] Where it all started: Attention! Be careful using Ethereum tokens.",
+    title: "[ETH reddit] Appeal to token developers on r/ethereum",
+    url: "https://www.reddit.com/r/ethereum/comments/6h17og/critical_problem_of_erc20_tokens_effect_appeal_to/",
+    date: "13.06.2017",
+    id: "appeal_to_developers",
+  },
+  {
+    title: "[ETH reddit] Description of the ERC-20 problems on r/ethereum",
+    url: "https://www.reddit.com/r/ethereum/comments/6h17aw/critical_problem_of_erc20_tokens/",
+    date: "13.06.2017",
+    id: "appeal_to_developers",
+  },
+  {
+    title: "[ETH reddit] ENS contract was deployed and instantly became a trap for more ERC-20 tokens",
+    url: "https://www.reddit.com/r/ethereum/comments/6e8y9o/the_ens_contract_becomes_token_holder_erc20/",
+    date: "30.05.2017",
+  },
+  {
+    title: "[ETH reddit] $77000 lost in Gnosis contract",
+    url: "https://www.reddit.com/r/ethereum/comments/6c68mw/new_record_holder_appears_lets_congratulate/",
+    date: "19.05.2017",
+  },
+  {
+    title: "[STORJ reddit] Legendary reply from STORJ dev: 'We know our users will lose money but we prefer to err on the side of well tested insecure code'",
+    url: "https://www.reddit.com/r/storj/comments/6ajjo3/comment/dhf95qz/?utm_source=reddit&utm_medium=web2x&context=3",
+    date: "11.05.2017",
+    id: "storj",
+  },
+  {
+    title: "[STORJ reddit] STORJ developers were warned about the issue with ERC-20 tokens",
+    url: "https://www.reddit.com/r/storj/comments/6ajjo3/attention_issues_of_the_upcoming_storj_migration/",
+    date: "11.05.2017",
+    id: "storj",
+  },
+  {
+    title: "[ETH reddit] Discussion with Ethereum Foundation members regarding ERC-223",
+    url: "https://www.reddit.com/r/ethereum/comments/66gr2a/metropolis_and_erc23_request_for/",
+    date: "20.04.2017",
+    id: "ethereum_foundation",
+  },  
+  {
+    title: "[medium] Vitalik Buterin responded to a question regarding ERC-223 standard",
+    url: "https://dexaran820.medium.com/response-to-vitaliks-speech-about-erc-23-ad240a27490f",
+    date: "17.04.2017",
+    id: "eth_reddit_ens",
+  },
+  {
+    title: "[ETH reddit] Where it all started: Attention! Be careful using Ethereum tokens.",
     url: "https://www.reddit.com/r/ethereum/comments/60ql37/attention_be_careful_using_ethereum_tokens/",
     date: "21.03.2017",
     id: "reddit_early_warnings",
   },
   {
-    title: "[ETC reddit] Explanation of the difference between ERC-20 and ERC-223",
+    title: "[ETC reddit] First explanation of the difference between ERC-20 and ERC-223",
     url: "https://www.reddit.com/r/EthereumClassic/comments/5ydst4/explanation_of_the_difference_between_erc20_and/",
     date: "9.03.2017",
   },
   {
     title: "[ETC reddit] Initial ERC-223 announcement on ETC public media",
     url: "https://www.reddit.com/r/EthereumClassic/comments/5xn8p8/new_erc_tokens_standard_proposal/",
-    date: "9.03.2017",
+    date: "5.03.2017",
     id: "tag_eip_submission",
   },
   {
     title: "[github] Token standard was created and submitted as Ethereum EIP 223",
     url: "https://github.com/ethereum/eips/issues/223",
-    date: "07.03.2017",
+    date: "5.03.2017",
     id: "tag_eip_submission",
   },
 
