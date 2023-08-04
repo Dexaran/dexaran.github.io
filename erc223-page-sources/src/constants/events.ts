5e6c7939f362013a1261e405b201586668bce8b9
Dexaran <dexaran820@gmail.com>
2023-08-05 01:15:48 +0400

    Update events.ts

--- erc223-page-sources/src/constants/events.ts
+++ erc223-page-sources/src/constants/events.ts
@@ -2,6 +2,26 @@ import {IRecentEvent} from "../types";
 
 export const events: IRecentEvent[] = [
   {
+    title: "ERC-223 copy was submitted as ECIP to Ethereum Classic",
+    url: "https://github.com/ethereumproject/ECIPs/issues/53",
+    date: "18.05.2017",
+    id: "tag_eip_submission",
+  },
+  {
+    title: "Reddit announcement: Attention! Be careful using Ethereum tokens.",
+    url: "https://www.reddit.com/r/ethereum/comments/60ql37/attention_be_careful_using_ethereum_tokens/",
+    date: "21.03.2017",
+    id: "reddit_early_warnings",
+  },
+  {
+    title: "Token standard was created and submitted as Ethereum EIP 223",
+    url: "https://github.com/ethereum/eips/issues/223",
+    date: "07.03.2017",
+    id: "tag_eip_submission",
+  },
+
+  /*
+  {
     title: "Lorem ipsum dolor sit amet.",
     url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c",
     date: "10.08.2024",
@@ -43,35 +63,6 @@ export const events: IRecentEvent[] = [
     url: "#",
     date: "10.08.2024",
     id: "tag1",
-  },
-  {
-    title: "Lorem ipsum dolor sit.",
-    url: "#",
-    date: "10.08.2024"
-  },
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur.",
-    url: "#",
-    date: "10.08.2024"
-  },
-  {
-    title: "Lorem ipsum dolor sit.",
-    url: "#",
-    date: "10.08.2024"
-  },
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur.",
-    url: "#",
-    date: "10.08.2024"
-  },
-  {
-    title: "Lorem ipsum dolor sit.",
-    url: "#",
-    date: "10.08.2024"
-  },
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    url: "#",
-    date: "10.08.2024"
   }
+  */
 ]
