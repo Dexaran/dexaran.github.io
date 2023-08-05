acb1befe6b20c6bdedd0774b52a817777fdd4be1
Dexaran <dexaran820@gmail.com>
2023-08-05 05:29:48 +0400

    Update articles.ts

--- erc223-page-sources/src/constants/articles.ts
+++ erc223-page-sources/src/constants/articles.ts
@@ -3,23 +3,26 @@ import {IArticle} from "../types";
 export const articles: IArticle[] = [
   {
     title: "Known problems of ERC-20 standard.",
-    description: "This rticle describes how bad ERC-20 is",
+    description: "Lack of transaction handling, pull transactions",
     url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c",
-    image: "image1.png"
+    image: "erc-20-known-problems.png"
   },
   {
     title: "ERC-20 approve & transferFrom methods pose a threat to safety of users funds",
-    description: "This article describes how bad ERC-20 is",
-    url: "https://dexaran820.medium.com/erc-20-approve-transferfrom-asset-transfer-method-poses-a-threat-to-users-funds-safety-ff7195127018"
+    description: "The article describes Pull tx VS Push tx approach",
+    url: "https://dexaran820.medium.com/erc-20-approve-transferfrom-asset-transfer-method-poses-a-threat-to-users-funds-safety-ff7195127018",
+    image: "approvals.png"
   },
   {
     title: "Callisto Security statement regarding ERC-20 standard.",
-    description: "...",
-    url: "https://callisto.network/erc-20-standard-callisto-network-security-department-statement/"
+    description: " - ",
+    url: "https://callisto.network/erc-20-standard-callisto-network-security-department-statement/",
+    image: "callisto-statement.png"
   },
   {
-    title: "TEST TEXT",
-    description: "TEST",
-    url: "https://google.com/"
-  }
+    title: "Cryptonomist: ERC-223",
+    description: " - ",
+    url: "https://en.cryptonomist.ch/2023/07/31/erc-223-lands-among-ethereum-standars/",
+    image: "cryptonomist.png"
+  },
 ];
