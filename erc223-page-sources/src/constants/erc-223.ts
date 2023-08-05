f7cc4b5eb9cfb8bf275337115bd5296c452e7691
Dexaran <dexaran820@gmail.com>
2023-08-05 05:09:04 +0400

    Update erc-223.ts

--- erc223-page-sources/src/constants/erc-223.ts
+++ erc223-page-sources/src/constants/erc-223.ts
@@ -2,9 +2,10 @@ import {IArticle} from "../types";
 
 export const erc223: IArticle[] = [
   {
-    title: "This is official source code",
-    description: "for any questions mail me dexaran@ethereumclassic.org",
-    url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
+    title: "ERC-223 Token source code",
+    description: "Reference implementation by the author of ERC-223 standard. Any questions: dexaran@ethereumclassic.org",
+    url: "https://github.com/Dexaran/ERC223-token-standard",
+    image: "source.png"
   },
   {
     title: "This is official source code",
