4a749d49567cc86355d5f2d8f81a00a1330ea5c2
gorbatiukcom <newgorbole@gmail.com>
2023-08-11 16:59:05 +0000

    Deploying to master from @ gorbatiukcom/gorbatiukcom.github.io@b123e2af9e7bae916f683ae2cbafbaef284c769f 🚀

---
+++ erc223-converter/_next/static/fc_3Iflu0gbX1WVznUPHm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8606a636127db993.css","static/chunks/pages/index-5c62fdfc43cb38c0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
