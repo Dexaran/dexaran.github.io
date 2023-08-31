66d561aa0a18361b19731d484cd8bc2dd81cbe39
Oleg Gorbatiuk <newgorbole@gmail.com>
2023-08-31 20:06:34 +0200

    Add prettier \ Fix code style

--- erc223-page-sources/src/types/index.ts
+++ erc223-page-sources/src/types/index.ts
@@ -1,19 +1,19 @@
 export type IArticle = {
-  title: string,
-  url: string,
-  description: string,
-  image?: string
-}
+  title: string;
+  url: string;
+  description: string;
+  image?: string;
+};
 
 export type IRecentEvent = {
-  title: string,
-  url?: string,
+  title: string;
+  url?: string;
   urls?: {
-    title: string,
-    url: string,  
-  }[],
-  date: string,
-  id?: string
-}
+    title: string;
+    url: string;
+  }[];
+  date: string;
+  id?: string;
+};
 
 export type IResourceTab = "articles" | "erc223" | "dev-sources";
