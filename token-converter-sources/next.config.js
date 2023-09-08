0c96524ac0b3398013fb94ac02a3371af3a605c8
Oleg Gorbatiuk <newgorbole@gmail.com>
2023-09-08 17:16:35 +0200

    Change erc-223-converter to token-converter

--- token-converter-sources/next.config.js
+++ token-converter-sources/next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? "/erc223-converter" : undefined,
+  basePath: isProd ? "/token-converter" : undefined,
 };
 
 module.exports = nextConfig;
