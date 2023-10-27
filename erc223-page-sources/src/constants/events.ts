5237360934509ed84d846ba030180ace757656fe
Dexaran <dexaran820@gmail.com>
2023-10-27 23:45:36 +0400

    Update events.ts

--- erc223-page-sources/src/constants/events.ts
+++ erc223-page-sources/src/constants/events.ts
@@ -2,12 +2,18 @@ import { IRecentEvent } from "../types";
 
 export const events: IRecentEvent[] = [
   {
+    title: "[article] Approval-related ERC-20 expoit on Uniswap",
+    url: "https://blockworks.co.in/uniswap-users-warned/",
+    date: "27.10.2023",
+    color: "#64DC84",
+  },
+  {
     title: "[github] Dev discussion regarding ERC-223 vs ERC-777 vs ERC-1363 implementations",
     url: "https://github.com/OpenZeppelin/openzeppelin-contracts/issues/2620#issuecomment-1769509345",
     date: "18.10.2023",
     color: "#64DC84",
   },
-    {
+  {
     title: "[github] EIP moderator proposed the removal of the optional 'standard' function from ERC-223 standard.",
     url: "https://github.com/ethereum/EIPs/pull/7856#issuecomment-1764921020",
     date: "16.10.2023",
