71959288bf6ef09d0eea2e7201a978773f7e7eff
Dexaran <dexaran820@gmail.com>
2023-11-01 23:57:50 +0400

    Update Header.tsx

--- erc20-losses-sources/src/components/Header/Header.tsx
+++ erc20-losses-sources/src/components/Header/Header.tsx
@@ -45,6 +45,15 @@ export const Header = () => {
           </a>
           .
         </p>
+        <p>
+          {`The problem was discovered in 2017. At the moment of disclosure there were $16,000 worth of tokens lost.`}
+        </p>
+        <p>
+          {`In 2018 there were approximately $1,000,000 worth of funds lost because of this ERC-20 standard flaw.`}
+        </p>
+        <p>
+          {`As of 11/1/2023 there are $228,722,284 worth of ERC-20 tokens lost. The amount is growing every day because there is no easy solution other than switching to a more secure standard.`}
+        </p>
         <h3>What this script does?</h3>
         <p>
           For each token contract address in a left input the script calculates how much of this
