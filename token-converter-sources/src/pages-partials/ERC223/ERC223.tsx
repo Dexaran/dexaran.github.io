6ab45ce4d3219846f2876cfc53c7153523c7b4fc
Oleg Gorbatiuk <newgorbole@gmail.com>
2024-02-23 10:54:35 +0100

    Add a plug to ERC223 page

--- token-converter-sources/src/pages-partials/ERC223/ERC223.tsx
+++ token-converter-sources/src/pages-partials/ERC223/ERC223.tsx
@@ -1,18 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import homeStyles from "../../styles/Home.module.scss";
 import styles from "./ERC223.module.scss";
 import clsx from "clsx";
 import { Content } from "@/constants/content";
+import { basePath } from "@/constants/build-config/isProd";
 
 export const ERC223 = () => {
+  const pageContent = (Content as any).erc223;
+  
   return (
     <>
-      <div className={styles.contentBlockHeader}>
-        <h1 className={clsx(homeStyles.h1, styles.h1)}>{Content.erc223.title}</h1>
-        <p className={clsx(homeStyles.description, styles.description)}>
-          {Content.erc223.description}
-        </p>
-      </div>
+      {pageContent ? (
+        <div className={styles.contentBlockHeader}>
+          <h1 className={clsx(homeStyles.h1, styles.h1)}>{pageContent.title}</h1>
+          <p className={clsx(homeStyles.description, styles.description)}>
+            {pageContent.description}
+          </p>
+        </div>
+      ) : (
+        <div className={styles.underConstructionContainer}>
+          <img
+            src={`${basePath}/under-construnction.svg`}
+            width="324px"
+            height="256px"
+            alt="Under Construnction"
+          />
+
+          <h1 className={clsx(homeStyles.h1, styles.h1)}>This page is under construction</h1>
+          <p className={clsx(homeStyles.description, styles.description)}>
+            ERC-223 page will launch soon. Come back and check it out.
+          </p>
+        </div>
+      )}
     </>
   );
 };
