3bfebbc305254a454529db580c8997500b242d0e
Oleg Gorbatiuk <newgorbole@gmail.com>
2024-11-09 03:18:36 +0100

    Try to fix favicon

--- erc20-losses-sources/src/pages/_document.tsx
+++ erc20-losses-sources/src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+import { basePath } from "@/constants/build-config/isProd";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link rel="shortcut icon" href={`${basePath}/static/favicon.ico`} />
       </Head>
       <body>
         <Main />
