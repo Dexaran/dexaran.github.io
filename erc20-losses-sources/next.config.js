485f4c2d21ebb80e6775afebffeee6831bbac57f
kostya12362 <rota199804@gmail.com>
2025-11-10 21:39:37 +0200

    Fix asset

--- erc20-losses-sources/next.config.js
+++ erc20-losses-sources/next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   reactStrictMode: true,
-  basePath: isProd ? "/erc20-losses" : undefined,
+  basePath: isProd ? "/erc20-losses" : '',
+  assetPrefix: isProd ? '/erc20-losses/' : '',
 };
 
 module.exports = nextConfig;
