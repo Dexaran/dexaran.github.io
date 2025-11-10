3d5ec832fa26cb343e57a23576e0a437adcbb7df
kostya12362 <rota199804@gmail.com>
2025-11-11 01:09:27 +0200

    Fix page

--- token-converter-sources/next.config.js
+++ token-converter-sources/next.config.js
@@ -2,8 +2,11 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
   reactStrictMode: true,
-  basePath: isProd ? "/token-converter" : undefined,
+  basePath: isProd ? '/token-converter' : '', //
+  assetPrefix: isProd ? '/token-converter/' : '', //
 };
 
 module.exports = nextConfig;
