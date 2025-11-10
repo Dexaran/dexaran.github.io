2e613781d8dffcb044f35c4fc8b4a11b45a783a1
kostya12362 <rota199804@gmail.com>
2025-11-10 20:03:23 +0200

    Fix action

--- erc20-losses-sources/next.config.js
+++ erc20-losses-sources/next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
   reactStrictMode: true,
   basePath: isProd ? "/erc20-losses" : undefined,
 };
