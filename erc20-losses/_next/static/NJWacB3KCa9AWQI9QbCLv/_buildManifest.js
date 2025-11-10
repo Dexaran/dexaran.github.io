095726b47b407e3e1d34f5840ab2b83b06087921
kostya12362 <37122338+kostya12362@users.noreply.github.com>
2025-11-10 18:04:50 +0000

    Deploying to master from @ kostya12362/dexaran.github.io@2e613781d8dffcb044f35c4fc8b4a11b45a783a1 🚀

---
+++ erc20-losses/_next/static/NJWacB3KCa9AWQI9QbCLv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9dc6a0d2419f19e5.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
